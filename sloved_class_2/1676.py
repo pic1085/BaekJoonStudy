8791b859cd45566f79964b14500572f6fdafe098
pic1085 <incheol1085@gmail.com>
2024-09-26 21:57:43 +0900

    1676 팩토리얼 문제 풀이입니다.

--- sloved_class_2/1676.py
+++ sloved_class_2/1676.py
@@ -1,12 +1,17 @@
 N = int(input())
 count = 0
 Fact_N = 1
+Fact_len = 0
 
 for i in range(1, N+1):
     Fact_N *= i
 
 Fact_N = str(Fact_N)
-for i in range(len(Fact_N)):
-    if Fact_N[i] == '0':
+Fact_len = len(Fact_N)
+
+for i in range(0, Fact_len):
+    if Fact_N[Fact_len - i - 1] == '0':
         count += 1
+    else:
+        break
 print (count)
