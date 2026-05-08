b07b30ada92da307d10c62da4aa3ccd9cff94dab
pic1085 <incheol1085@gmail.com>
2026-05-08 10:22:32 +0900

    문제 수정

--- programmers/LV0/flag.py
+++ programmers/LV0/flag.py
@@ -1,19 +1,11 @@
-# def solution(a, b, flag):
-#     answer = 0
-#     if flag == "True":
-#         answer = a + b
-#     else:
-#         answer = a - b
-#     return answer
-
+def solution(a, b, flag):
+    answer = 0
+    if flag == True:
+        answer = a + b
+    else:
+        answer = a - b
+    return answer
 a = -4
 b = 7
 flag = "true"
-
-
-def solution(a, b, flag):
-    answer = 0
-    return answer
-
-# print(solution(a, b, flag))
-print(flag)
+print(solution(a, b, flag))
