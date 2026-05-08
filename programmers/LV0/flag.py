713055574a6603333c220452af07c3a0da44a27d
pic1085 <incheol1085@gmail.com>
2026-05-08 10:22:14 +0900

    programmers LV0 flag 문제 풀이

---
+++ programmers/LV0/flag.py
@@ -0,0 +1,19 @@
+# def solution(a, b, flag):
+#     answer = 0
+#     if flag == "True":
+#         answer = a + b
+#     else:
+#         answer = a - b
+#     return answer
+
+a = -4
+b = 7
+flag = "true"
+
+
+def solution(a, b, flag):
+    answer = 0
+    return answer
+
+# print(solution(a, b, flag))
+print(flag)
