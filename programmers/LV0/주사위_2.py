cc8bcdb7f991f07d1c7fab0d62f593f8cd1d0b0e
pic1085 <incheol1085@gmail.com>
2026-05-09 15:49:18 +0900

    programmers LV0 주사위 문제풀이

---
+++ programmers/LV0/주사위_2.py
@@ -0,0 +1,14 @@
+def solution(a, b, c):
+    answer = 0
+    if a != b and b != c and a != c:
+        answer += a + b + c
+    elif a == b and b == c and c == a:
+        answer += (a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3)
+    else:
+        answer += (a + b + c) * (a**2 + b**2 + c**2)
+    return answer
+
+a = 4
+b = 4
+c = 4
+print(solution(a, b, c))
